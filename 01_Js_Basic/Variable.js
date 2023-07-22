8e2c3e37a976560f7e7c134a29c4039311ed0694
aashishdani <109866017+aashishdani@users.noreply.github.com>
2023-07-22 10:30:03 +0000

    variable done

---
+++ 01_Js_Basic/Variable.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountName = "rohit"
+var accountEmail = "rohit@gmail.com"
+// let accountCity;  undefined
+
+// accountId = 54321  // not allowed const to update
+
+console.log(accountId)
+// console.log(accountCity); value is undefined if not initialize in variable
+
+// mostly used let 
+console.table([accountId,accountName,accountEmail])
+
+let personName = "aman"
+carName = "volvo"
+price = 200000
+
+console.table([personName,carName,price])
